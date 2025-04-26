b0aa95a0d9b6130c13ace927d88075a4f0818686
Tim Bossuyt <tim.bossuyt@outlook.com>
2025-04-27 00:05:16 +0200

    Writing test files

--- programs/favorites/src/lib.rs
+++ programs/favorites/src/lib.rs
@@ -2,15 +2,76 @@ use anchor_lang::prelude::*;
 
 declare_id!("HhQUdUs3NXAgUu8oB3B2SGQ31PYkJrgN8m4v5Af1rX58");
 
+pub const ANCHOR_DISCRIMINATOR_SIZE: usize = 8;
+
 #[program]
 pub mod favorites {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn say_hello(ctx: Context<SayHello>) -> Result<()> {
         msg!("Greetings from: {:?}", ctx.program_id);
         Ok(())
     }
+
+    pub fn set_favorites(ctx: Context<SetFavorites>, 
+        number: u64, color: String, hobbies: Vec<String>) -> Result<()> {
+            let user_pubkey = ctx.accounts.user.key();
+            msg!("Greetings from {}", ctx.program_id);
+
+            msg!(
+                "User {user_pubkey}'s favorite number is {number}, favorite color is: {color}",
+            );
+
+            msg!(
+                "User's hobbies are: {:?}",
+                hobbies
+            ); 
+            
+            // Updating these values on the blockchain
+            ctx.accounts.favorites.set_inner(Favorites {
+                number,
+                color,
+                hobbies
+            });
+
+
+            Ok(())
+
+        }
+
+    
+}
+
+
+// What we will put inside the Favorites PDA
+#[account]
+#[derive(InitSpace)]
+pub struct Favorites {
+    pub number: u64,
+
+    #[max_len(50)]
+    pub color: String,
+
+    #[max_len(5, 50)]
+    pub hobbies: Vec<String>
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct SayHello {}
+
+#[derive(Accounts)]
+pub struct SetFavorites<'info> {
+    #[account(mut)]
+    pub user: Signer<'info>,
+
+    #[account(
+        init_if_needed, 
+        payer = user, 
+        space = ANCHOR_DISCRIMINATOR_SIZE + Favorites::INIT_SPACE, 
+        seeds=[b"favorites", user.key().as_ref()],
+        bump
+    )]
+    pub favorites: Account<'info, Favorites>,
+
+    pub system_program: Program<'info, System>,
+}
--- tests/favorites.ts
+++ tests/favorites.ts
@@ -1,16 +1,55 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { Favorites } from "../target/types/favorites";
+import { assert } from 'chai';
 
 describe("favorites", () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
-
+  const provider = anchor.AnchorProvider.env();
   const program = anchor.workspace.favorites as Program<Favorites>;
+  
+  const user = (provider.wallet as anchor.Wallet).payer;
+  const someRandomGuy = anchor.web3.Keypair.generate();
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
+  // Here's what we want to write to the blockchain
+  const favoriteNumber = new anchor.BN(6);
+  const favoriteColor = 'purple';
+  const favoriteHobbies = ['skiing', 'skydiving', 'biking'];
+
+  before(async () => {
+    const balance = await provider.connection.getBalance(user.publicKey);
+    const balanceInSOL = balance / anchor.web3.LAMPORTS_PER_SOL;
+    const formattedBalance = new Intl.NumberFormat().format(balanceInSOL);
+    console.log(`Balance: ${formattedBalance} SOL`);
+  });
+
+  it("Saying Hello!", async () => {
+    const tx = await program.methods.sayHello().rpc();
     console.log("Your transaction signature", tx);
   });
+
+  it("Setting user favorites on the blockchain", async () => {
+    const tx = await program.methods.setFavorites(
+      favoriteNumber,
+      favoriteColor,
+      favoriteHobbies
+    )
+    .signers([user])
+    .rpc();
+
+    // Retrieve the associated PDA --> [pubkey, bump-seed] --> at index 0 of return value
+    const favorites_pda_key = anchor.web3.PublicKey
+    .findProgramAddressSync(
+      [Buffer.from('favorites'), user.publicKey.toBuffer()], 
+      program.programId)[0];
+
+    const data = await program.account.favorites.fetch(favorites_pda_key);
+
+    // And make sure it matches!
+    assert.equal(data.color, favoriteColor);
+    assert.equal(data.number.toString(), favoriteNumber.toString());
+    assert.deepEqual(data.hobbies, favoriteHobbies);
+  })
+
 });
